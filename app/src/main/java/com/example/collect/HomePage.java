cad61130f6b50db5f65050185c58f4f7c0ae51dd
Lesiba <lesiba.mabitsela@fnb.co.za>
2021-06-01 14:15:52 +0200

    Home page, Navigation and more ...

--- app/src/main/java/com/example/collect/HomePage.java
+++ app/src/main/java/com/example/collect/HomePage.java
@@ -1,14 +1,32 @@
 package com.example.collect;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class HomePage extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView nav_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer);
+        nav_view = findViewById(R.id.nav_view);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
     }
 }

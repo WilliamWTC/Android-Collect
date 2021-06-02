f1b480ee58735af662a195a283d1a6e9affffd6a
Lesiba <lesiba.mabitsela@fnb.co.za>
2021-06-02 10:28:58 +0200

    Example category setup.

--- app/src/main/java/com/example/collect/HomePage.java
+++ app/src/main/java/com/example/collect/HomePage.java
@@ -1,19 +1,30 @@
 package com.example.collect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.collect.model.Adapter;
 import com.google.android.material.navigation.NavigationView;
 
-public class HomePage extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    RecyclerView objectList;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
     NavigationView nav_view;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +35,35 @@ public class HomePage extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer);
         nav_view = findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(this);
+        objectList = findViewById(R.id.objectlist);
+
         toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
+
+        List<String> titles = new ArrayList<>();
+        List<String> content = new ArrayList<>();
+
+        titles.add("First Note");
+        content.add("first content");
+
+        titles.add("2nd Note");
+        content.add("2nd content");
+
+        adapter = new Adapter(titles, content);
+        objectList.setLayoutManager(new LinearLayoutManager(this));
+        objectList.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            default:
+                Toast.makeText(this, "TO DO", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/collect/model/Adapter.java
@@ -0,0 +1,64 @@
+package com.example.collect.model;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.collect.R;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    List<String> titles;
+    List<String> objects;
+
+    public  Adapter(List<String> titles, List<String> objects){
+        this.titles = titles;
+        this.objects = objects;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.collections,parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.objectTitle.setText(titles.get(position));
+        holder.objectContent.setText(objects.get(position));
+
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Item clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView objectTitle, objectContent;
+        View view;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            objectTitle = itemView.findViewById(R.id.title);
+            objectContent = itemView.findViewById(R.id.content);
+            view = itemView;
+        }
+    }
+}
